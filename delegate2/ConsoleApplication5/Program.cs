e01d7df4fce0264eacbe034093840ce5258d5587
kabrate <937160666@qq.com>
2017-04-13 20:28:15 +0800

    delegate2

---
+++ delegate2/ConsoleApplication5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+delegate int NumberChange(int n);
+namespace DelegateApp1
+{
+    class TestDelagate
+    {
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            //创建委托实例
+            NumberChange nc;
+            NumberChange nc1 = new NumberChange(AddNum);
+            NumberChange nc2 = new NumberChange(MultNum);
+            nc = nc1;
+            nc += nc2;
+            //调用多播
+            nc(5);
+            Console.WriteLine("value of num:{0}",getNum());
+            Console.ReadKey();
+        }
+    }
+}
+
